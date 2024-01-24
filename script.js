06e9bf4659b0aa5057f648e800d12c42102c7d8d
Stheven <sthevenalvess@gmail.com>
2024-01-24 16:02:45 -0300

    feat: button next slide

--- script.js
+++ script.js
@@ -11,16 +11,22 @@ const images = [
 
 const containerItems = document.querySelector('#container-items');
 
+
 const loadImages = (images, container) => {
     images.forEach(image => {
-    container.innerHTML += `
-    <div class='item'>
+        container.innerHTML += `
+        <div class='item'>
         <img src='${image.url}'>
-    </div>
-    `
+        </div>
+        `
     })
 }
-
-
-
 loadImages(images, containerItems)
+let items = document.querySelectorAll('.item');
+
+const next = () => {
+    containerItems.appendChild(items[0]);
+    items = document.querySelectorAll('.item');
+}
+
+document.querySelector('#next').addEventListener('click', next);
